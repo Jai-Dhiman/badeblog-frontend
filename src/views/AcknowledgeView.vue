27dee76207c57fca55d7a8b6d22e8aa373470a68
JaiDhiman <jaidhiman2000@gmail.com>
2024-12-16 14:46:57 -0800

    added paragraphs to acknoweledgement

--- src/views/AcknowledgeView.vue
+++ src/views/AcknowledgeView.vue
@@ -9,23 +9,33 @@
               It’s a story which has seen many ups and downs in its long journey. My first attempt
               to write a short-story was in my late teen years that could not be continued for my
               personal circumstances. Nonetheless, the thirst of commitment to ink the paper did not
-              die and a spark somehow managed to appear before my eyes every now and then. When I
-              came into the job, I noticed my interest rising; and I started composing poetry in
-              free verse. My compositions got adequate appreciations from my colleagues and family
-              members. Still, it could not be carried on, though I did try on and off. After
-              superannuation from job, I penned up many short-stories and poetry including
+              die and a spark somehow managed to appear before my eyes every now and then.
+            </p>
+            <p class="mb-4 text-xl">
+              When I came into the job, I noticed my interest rising; and I started composing poetry
+              in free verse. My compositions got adequate appreciations from my colleagues and
+              family members. Still, it could not be carried on, though I did try on and off.
+            </p>
+            <p class="mb-4 text-xl">
+              After superannuation from job, I penned up many short-stories and poetry including
               traditional as well as free verse and two liners. My friends and family members were
               again inclined to admire them too much and continuously encouraged me to write more
               and have a compilation in any form, but destiny had decided some other path to be
-              followed by me. My interest of writing re-instilled when I came to the USA to visit my
-              elder son and daughter and their families; and my grand-daughter asked me to write on
-              certain topics in English language, though my work carried out earlier is in Hindi
-              language. With this, the issue of compilation of my authoring came up for discussion
-              when one of my grandsons offered to create a website; and this beautiful internet site
-              is before you. I take this opportunity to express my heart-felt gratitude to my
-              colleagues, friends, my wife, sons, daughters-in-law, daughter, son-in-law,
-              grandchildren and other promoters, and wish them all the best. DHIMAN PR
+              followed by me.
             </p>
+            <p class="mb-4 text-xl">
+              My interest of writing re-instilled when I came to the USA to visit my elder son and
+              daughter and their families; and my grand-daughter asked me to write on certain topics
+              in English language, though my work carried out earlier is in Hindi language. With
+              this, the issue of compilation of my authoring came up for discussion when one of my
+              grandsons offered to create a website; and this beautiful internet site is before you.
+            </p>
+            <p class="mb-4 text-xl">
+              I take this opportunity to express my heart-felt gratitude to my colleagues, friends,
+              my wife, sons, daughters-in-law, daughter, son-in-law, grandchildren and other
+              promoters, and wish them all the best.
+            </p>
+            <p class="mb-4 text-xl">DHIMAN PR</p>
           </div>
         </div>
       </div>
